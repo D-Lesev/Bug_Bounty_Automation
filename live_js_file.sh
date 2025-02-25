6da4a5f1734f63a14b40f6a1cb465b4993c2487b
DLESEV <kali@kali>
2025-02-25 17:51:15 +0200

    Changes

--- live_js_file.sh
+++ live_js_file.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if [ -z "$1" ] || [ -z "$2" ];  then
-    echo -e "\033[1;31m[-] Error! Usage: $0 <main domain> <domain we want>\033[0m"
+    echo -e "\033[1;31m[-] Error! Usage: $0 example.com abc.example.com\033[0m"
     exit 1
 fi
 
 
 gau "$1" | grep -E "^https://$2/.*\.js(\?.*)?$" | httpx -mc 200 > "live_js_$2"
+
+
+echo -e "\n\033[38;5;10m[+] Done!\033[0m"
